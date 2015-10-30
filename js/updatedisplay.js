29b052544a06673fab20b1feabd6aa20fa92dbee
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2015-10-30 05:10:55 -0600

    Add speaker notes plugin

--- js/updatedisplay.js
+++ js/updatedisplay.js
@@ -81,7 +81,17 @@ Reveal.initialize({
     // Amount to move parallax background (horizontal and vertical) on slide change
     // Number, e.g. 100
     parallaxBackgroundHorizontal: '',
-    parallaxBackgroundVertical: ''
+    parallaxBackgroundVertical: '',
+
+    // Optional reveal.js plugins
+    dependencies: [
+        { src: 'reveal.js/lib/js/classList.js', condition: function() { return !document.body.classList; } },
+        //{ src: 'reveal.js/plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+        //{ src: 'reveal.js/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+        { src: 'reveal.js/plugin/highlight/highlight.js', async: true, condition: function() { return !!document.querySelector( 'pre code' ); }, callback: function() { hljs.initHighlightingOnLoad(); } },
+        { src: 'reveal.js/plugin/zoom-js/zoom.js', async: true },
+        { src: 'reveal.js/plugin/notes/notes.js', async: true }
+    ]
 }
 );
 
