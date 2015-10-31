ea53bb375a2ba82cb09fd22c2df8b8d0537f895f
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2015-10-31 14:33:47 -0600

    Add slide and fix story

--- js/data0.js
+++ js/data0.js
@@ -55,6 +55,8 @@ var data_demo0_3 = {"directed": true, "HEAD": "1b66dad086ed9fafe3c1adab66a8bc9d4
             {"source": 0, "target": 1}, 
             {"source": 1, "target": 2},
             {"source": 2, "target": 4},
+            {"source": 3, "target": 5},
+            {"source": 1, "target": 3},
     ], 
     "multigraph": false, 
     "graph": [], 
@@ -68,6 +70,7 @@ var data_demo0_3 = {"directed": true, "HEAD": "1b66dad086ed9fafe3c1adab66a8bc9d4
             {"message": "Test feature 1b", "id": "7t843ab054ed9faf432on32fd0k0839nc02423n3", "pos": [400.0, 200+400.0]},
             {"message": "Test feature 2a", "id": "98jn32n3932n01n012n0214n1201n10s021h102n", "pos": [600.0, 200+400.0]}, 
             {"message": "Test feature 2b", "id": "1b66dad086ed9fafe3c1adab66a8bc9d4f2c43b2", "pos": [400.0, 200+600.0]},
+            {"message": "Test feature 2b", "id": "0b66dad086ed9fafe3c1adab66a8bc9d4f2c43b2", "pos": [600.0, 200+600.0]},
     ]
 };
 
@@ -75,21 +78,20 @@ var data_demo0_final = {"directed": true, "HEAD": "590e14f6c62f904af769a4143fe06
     "links": [
             {"source": 0, "target": 1}, 
             {"source": 1, "target": 2},
-            {"source": 2, "target": 4},
-            {"source": 4, "target": 5},
-            {"source": 0, "target": 5},
+            {"source": 2, "target": 3},
+            {"source": 3, "target": 4},
+            {"source": 0, "target": 4},
     ], 
     "multigraph": false, 
     "graph": [], 
     "labels": ["master",], 
-    "master": "3a253b5c7ff62479e333c4f60dc41d46863e8905", 
+    "master": "590e14f6c62f904af769a4143fe067063cd5e684", 
     "feature1": "0b66dad086ed9fafe3c1adab66a8bc9d4f2c43b2", 
-    "feature2": "0b66dad086ed9fafe3c1adab66a8bc9d4f2c43b2", 
+    "feature2": "1b66dad086ed9fafe3c1adab66a8bc9d4f2c43b2", 
     "nodes": [
             {"message": "Initial commit", "id": "3a253b5c7ff62479e333c4f60dc41d46863e8905", "pos": [200, 200+0]}, 
             {"message": "Test feature 1a", "id": "1ddefaae6634d68350f4b738702f1103442c1af4", "pos": [400.0, 200+200.0]}, 
             {"message": "Test feature 1b", "id": "7t843ab054ed9faf432on32fd0k0839nc02423n3", "pos": [400.0, 200+400.0]},
-            {"message": "Test feature 2a", "id": "98jn32n3932n01n012n0214n1201n10s021h102n", "pos": [600.0, 200+400.0]}, 
             {"message": "Test feature 2b", "id": "1b66dad086ed9fafe3c1adab66a8bc9d4f2c43b2", "pos": [400.0, 200+600.0]},
             {"message": "Merge commit", "id": "590e14f6c62f904af769a4143fe067063cd5e684", "pos": [200.0, 200+800.0]},
     ]
